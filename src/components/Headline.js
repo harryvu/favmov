27980c242da3904997f997fbcb50e9a33dc93f9b
Harry Vu <harryvu@outlook.com>
2019-06-04 12:59:18 -0700

    Added first function component using react hooks.

---
+++ src/components/Headline.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+
+const Headline = () => {
+    const [greeting, setGreeting] = useState(
+        'Hello Function Component!'
+    );
+
+    return (
+        <div>
+            <h1>{greeting}</h1>
+            <input type="text" value={greeting} onChange={event => setGreeting(event.target.value)} />
+        </div>
+    );
+};
+
+export default Headline;
